ec45444f1b66bda78fb33d53589a904e7cd5df7e
AhmedHassan-004 <ahmedg2006ghafar@gmail.com>
2026-05-15 15:14:50 +0500

    Task 3

--- Source.cpp
+++ Source.cpp
@@ -24,42 +24,102 @@
 //	}
 //	return 0;
 //}
-#include<iostream>
+//#include<iostream>
+//using namespace std;
+//void test(int a,char c);
+//int main()
+//{
+//	int a;
+//	char c;
+//	cout << "Testing Multiple Catches" << endl;
+//	cout << "Enter the integer value:";
+//	cin >> a;
+//	cout << "Enter the character:";
+//	cin >> c;
+//	test(a, c);
+//	return 0;
+//}
+//void test(int a, char c)
+//try
+//{
+//	if (a > 0)
+//	{
+//		throw a;
+//	}
+//	else
+//	{
+//		throw c;
+//	}
+//
+//   
+//	catch(int a)
+//	{
+//		cerr << "Caught an integer: " << a << endl;
+//	}
+//	catch (char c)
+//	{
+//		cerr << "Caught a character: " << c << endl;
+//
+//
+#include <iostream>
 using namespace std;
-void test(int a,char c);
-int main()
-{
-	int a;
-	char c;
-	cout << "Testing Multiple Catches" << endl;
-	cout << "Enter the integer value:";
-	cin >> a;
-	cout << "Enter the character:";
-	cin >> c;
-	test(a, c);
-	return 0;
-}
-void test(int a, char c)
-try
-{
-	if (a > 0)
-	{
-		throw a;
-	}
-	else
-	{
-		throw c;
-	}
+
+// ===== Custom Exception Class =====
+class OutOfRangeException {
+private:
+    string message;
+
+public:
+    OutOfRangeException(string msg);
+    string getMessage();
+};
+
+OutOfRangeException::OutOfRangeException(string msg) {
+    message = msg;
 }
 
-   
-	catch(int a)
-	{
-		cerr << "Caught an integer: " << a << endl;
-	}
-	catch (char c)
-	{
-		cerr << "Caught a character: " << c << endl;
+string OutOfRangeException::getMessage() {
+    return message;
+}
+float calculateAverage(int m1, int m2, int m3, int m4);
 
+int main() {
+    int a, b, c, d;
+    cout << "Enter marks for 4 courses: ";
+    cin >> a >> b >> c >> d;
 
-	}
+    try {
+        float avg = calculateAverage(a, b, c, d);
+        cout << "Average = " << avg << endl;
+    }
+    catch (OutOfRangeException e) {
+        cout << "Error: " << e.getMessage() << endl;
+    }
+
+    return 0;
+}
+
+/*
+ * Preconditions:
+ *   Four integer arguments must be provided
+ *   Each mark must be in range 0 to 100 
+ *
+ * Postconditions:
+ *   Returns the average of the four marks as a float
+ *   If any mark is out of range, exception is thrown
+ *
+ * Invariants:
+ *   Sum is always divided by 4
+ *   No out-of-range mark is used in calculation
+ *
+ * Exceptions:
+ *   Throws OutOfRangeException if any mark < 0 or > 100
+ */
+float calculateAverage(int m1, int m2, int m3, int m4) {
+    if (m1 < 0 || m1 > 100) throw OutOfRangeException("Mark is out of valid range (0-100).");
+    if (m2 < 0 || m2 > 100) throw OutOfRangeException("Mark is out of valid range (0-100).");
+    if (m3 < 0 || m3 > 100) throw OutOfRangeException("Mark is out of valid range (0-100).");
+    if (m4 < 0 || m4 > 100) throw OutOfRangeException("Mark is out of valid range (0-100).");
+
+    return (m1 + m2 + m3 + m4) / 4.0;
+}
